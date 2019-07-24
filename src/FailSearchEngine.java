297758a1e226c382eb9f29c3a4726af21031e4f5
Deniska <lishchuk.denis@fornova.net>
2019-07-24 21:01:46 +0300

    fix binary search

--- src/FailSearchEngine.java
+++ src/FailSearchEngine.java
@@ -6,53 +6,67 @@ public class FailSearchEngine implements Failable {
         List<NodeItem> nodeItems = clusterItem.getNodeItems();
         if (nodeItems.isEmpty()) throw new Exception("Cannot perform search in empty array!");
 
-        int midIndex = nodeItems.size() / 2;
-        NodeItem midNode = nodeItems.get(midIndex);
 
         int iteration = 0, startIndex, endIndex;
-        int previousMidIndex = nodeItems.size();
-        boolean previousStatus = false;
-        NodeItem previousItem = midNode;
+        int previousStartIndex = 0, previousEndIndex = nodeItems.size();
+        int lastDisconnectedNodeIndex = 0;
+        boolean previousStatus;
+        NodeItem previousItem, lastDisconnectedNode = null;
+
+        int midIndex = nodeItems.size() / 2;
+        int previousMidIndex = midIndex;
+        NodeItem midNode = nodeItems.get(midIndex);
 
         // Binary search
-        while (iteration <= nodeItems.size()) {
-            boolean isFailed;
-            isFailed = isFailed(clusterItem, midNode.getParentId(), midNode.getId());
+        boolean isFailed;
+        isFailed = isFailed(clusterItem, midNode.getParentId(), midNode.getId());
+        iteration++;
 
+        while (iteration <= nodeItems.size()) {
             if (isFailed) {
-                startIndex = iteration > 1 ? midIndex : 0;
+                lastDisconnectedNodeIndex = previousMidIndex;
+                lastDisconnectedNode = midNode;
+
+                startIndex = previousStartIndex;
                 endIndex = midIndex;
-                previousMidIndex = midIndex;
-                midIndex = (endIndex - startIndex) / 2;
+                midIndex = startIndex + (endIndex - startIndex) / 2;
             } else {
                 startIndex = midIndex;
-                endIndex = previousMidIndex;
+                endIndex = previousEndIndex;
                 midIndex = startIndex + ((endIndex - startIndex) / 2);
             }
 
-            // Boundary condition
-            if (previousStatus && endIndex - startIndex == 2 && !isFailed) {
-                midNode = nodeItems.get(midIndex);
-                isFailed = isFailed(clusterItem, midNode.getParentId(), midNode.getId());
-                if (!isFailed) return previousItem;
-            }
-
-            previousStatus = isFailed;
+            // Remember previous state to use it later
+            previousMidIndex = midIndex;
+            previousStartIndex = startIndex;
+            previousEndIndex = endIndex;
             previousItem = midNode;
+            previousStatus = isFailed;
 
+            // Get state of a new mid index node
             midNode = nodeItems.get(midIndex);
             isFailed = isFailed(clusterItem, midNode.getParentId(), midNode.getId());
             iteration++;
 
+            // Case when our first mid index = searched node
+            if (!isFailed && Math.abs(lastDisconnectedNodeIndex - midIndex) <= 1) {
+                System.out.println("\nWe've found a disconnected node in " + iteration + " iteration(s)");
+                return lastDisconnectedNode;
+            }
+
             boolean changedStatusOnBoundary = (!previousStatus && isFailed) || (previousStatus && !isFailed);
-            if (changedStatusOnBoundary && endIndex - startIndex <= 1) {
-                return previousItem; // We've found a node
+            if (changedStatusOnBoundary) {
+                if (!isFailed && Math.abs(previousEndIndex - midIndex) <= 1) { // Distance between nodes <= 1 node
+                    System.out.println("\nWe've found a disconnected node in " + iteration + " iteration(s)");
+                    return previousItem;
+                }
             }
 
             if (iteration == nodeItems.size()) {
                 if (endIndex - startIndex == 1) { // Reached the end of binary search without any match found
                     throw new Exception("Cannot find disconnected node, all nodes working fine");
                 } else {
+                    System.out.println("\nWe've found a disconnected node in " + iteration + " iteration(s)");
                     return nodeItems.get(0); // Case when all nodes are disconnected
                 }
             }
